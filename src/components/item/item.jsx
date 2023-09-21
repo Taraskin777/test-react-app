a02aff011d1595850228182f4b27b373a1077e74
Taras Noga <taraskin2012@gmail.com>
2023-09-22 00:07:57 +0300

    add check for comments

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -1,13 +1,15 @@
 import "./item.css";
 
 export default function Item(props) {
-  const { title, removeItem, id, toggleItem, checked } = props;
-
-
+  const { title, removeItem, id, toggleItem, checked, comments } = props;
 
   return (
-    <li className={`item ${checked ? 'checked' : ''}`} onClick={()=>toggleItem(id)}>
+    <li
+      className={`item ${checked ? "checked" : ""}`}
+      onClick={() => toggleItem(id)}
+    >
       <div>{title}</div>
+      {comments ? comments.length : 0}
       <button onClick={() => removeItem(id)}>Delete</button>
     </li>
   );
